882054d03cbe2358c50b8d4d424e6d3304859fdf
zuksi <ne_ksenia@mail.ru>
2020-03-25 14:21:28 +0300

    -

---
+++ lesson-5/scrapy/jobparser/items.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+
+# Define here the models for your scraped items
+#
+# See documentation in:
+# https://docs.scrapy.org/en/latest/topics/items.html
+
+import scrapy
+
+
+class JobparserItem(scrapy.Item):
+    # define the fields for your item here like:
+    _id = scrapy.Field()
+    name = scrapy.Field()
+    salary = scrapy.Field()
+    salary_min = scrapy.Field()
+    salary_max = scrapy.Field()
+    link = scrapy.Field()
+    site = scrapy.Field()
+
+
+# class superjobItem(scrapy.Item):
