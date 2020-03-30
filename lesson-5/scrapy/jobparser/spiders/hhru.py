f804ee996b1fc78b1f2f6749d201d9d15d6039e3
zuksi <ne_ksenia@mail.ru>
2020-03-30 14:07:46 +0300

    -

--- lesson-5/scrapy/jobparser/spiders/hhru.py
+++ lesson-5/scrapy/jobparser/spiders/hhru.py
@@ -2,6 +2,7 @@
 import scrapy
 from scrapy.http import HtmlResponse
 from jobparser.items import JobparserItem
+from scrapy.loader import ItemLoader
 
 
 class HhruSpider(scrapy.Spider):
@@ -18,14 +19,14 @@ class HhruSpider(scrapy.Spider):
         yield response.follow(next_page, callback=self.parse)
 
         vac_list = response.xpath("//a[@class='bloko-link HH-LinkModifier']/@href").extract()
-        # vacansy = response.css('div.vacancy-serp div.vacancy-serp-item div.vacancy-serp-item__row_header a.bloko-link::attr(href)')
 
         for link in vac_list:
             yield response.follow(link, callback=self.vacansy_parse)
 
     def vacansy_parse(self, response: HtmlResponse):
-        name = response.xpath("//h1[@class='bloko-header-1']/text()").extract()[0]
-        salary = response.xpath("//span[@class='bloko-header-2 bloko-header-2_lite']/text()").extract()
-        vacansy_link = response.request.url
-        vacansy_site = response.request.url
-        yield JobparserItem(name=name, salary=salary, link=vacansy_link, site=vacansy_site)
+        loader = ItemLoader(item=JobparserItem(), response=response)
+        loader.add_xpath('name',"//h1[@class='bloko-header-1']/text()")
+        loader.add_xpath('salary', "//span[@class='bloko-header-2 bloko-header-2_lite']/text()")
+        loader.add_value('link',response.request.url)
+        loader.add_value('site', 'HH.ru')
+        yield loader.load_item()
