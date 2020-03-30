20758fed458d44f1a52af74eef4bdd3250ea6c6c
zuksi <ne_ksenia@mail.ru>
2020-03-30 14:07:27 +0300

    -

--- lesson-5/scrapy/jobparser/items.py
+++ lesson-5/scrapy/jobparser/items.py
@@ -6,17 +6,17 @@
 # https://docs.scrapy.org/en/latest/topics/items.html
 
 import scrapy
-
+from scrapy.loader.processors import TakeFirst
 
 class JobparserItem(scrapy.Item):
     # define the fields for your item here like:
     _id = scrapy.Field()
-    name = scrapy.Field()
+    name = scrapy.Field(output_processor=TakeFirst())
     salary = scrapy.Field()
     salary_min = scrapy.Field()
     salary_max = scrapy.Field()
-    link = scrapy.Field()
-    site = scrapy.Field()
+    link = scrapy.Field(output_processor=TakeFirst())
+    site = scrapy.Field(output_processor=TakeFirst())
 
 
 # class superjobItem(scrapy.Item):
